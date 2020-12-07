7ab9d7a60f3741e240fde8a5fe44a488a867bdaf
Jhonata <jhonata.tenorio2526@gmail.com>
2020-12-07 00:18:07 -0300

    1.3

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,4 @@
-function res(){
-    alert("Vc devia pesquisar mais")
+const checked = (id) => {
+    let classes = id.className.split(' ');
+    classes[1] ? id.classList.remove(classes[1]) : id.classList.add("checked"); 
 }
