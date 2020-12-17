bcd84bb1d758f83d803c029dbc1d2a4da100f62f
Jhn <jhonata.tenorio2526@gmail.com>
2020-12-17 20:00:18 -0300

    server.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,3 +2,4 @@ const checked = (id) => {
     let classes = id.className.split(' ');
     classes[1] ? id.classList.remove(classes[1]) : id.classList.add("checked"); 
 }
+console.log("AAA");
---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views", "index.html"));
+});
+
+app.listen(port);
