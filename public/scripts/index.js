c5732fed6d59ed8538c986fccf32ea2048ad15b9
Jhn <jhonata.tenorio2526@gmail.com>
2020-12-17 22:47:17 -0300

    input verify

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,3 +2,10 @@ const checked = (id) => {
     let classes = id.className.split(' ');
     classes[1] ? id.classList.remove(classes[1]) : id.classList.add("checked"); 
 }
+
+const validate = () => {
+    const PREFIX = "https://chat.whatsapp.com/";
+    const link = document.getElementById("linkWpp");
+    if(!link.value.startsWith(PREFIX))
+        link.classList.add('invalid');
+}
