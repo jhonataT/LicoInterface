98ed0b93ec3a18cb7dbf2bb296850e289f3957c6
Jhn <jhonata.tenorio2526@gmail.com>
2020-12-20 11:54:08 -0300

    add link and commands to the Database

--- src/db.js
+++ src/db.js
@@ -11,4 +11,29 @@ sequelize.authenticate()
 })
 .catch( err => {
     console.log(err);
-})
+});
+
+class Database {
+    static table(){ // createTable;
+        const Grupos = sequelize.define('groupCommandsAndLinks', {
+            link: {
+                type: Sequelize.STRING
+            },
+            commands: {
+                type: Sequelize.STRING
+            }
+        });
+        // Grupos.sync( { force: true } ); // Criar / substituir uma table;
+        return Grupos;
+    }
+
+    static async addItem(linkGroup, cmds){
+        const Grupos = await this.table();
+        Grupos.create( {
+            link: `${linkGroup}`,
+            commands: `${cmds}`
+        })
+    }
+}
+
+module.exports = Database;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { link } = require('fs');
 const bodyParser = require('body-parser');
+const Database = require('./db');
 const path = require('path');
 const app = express();
 const port = '3000';
@@ -16,8 +17,11 @@ app.get('/', (req, res) => {
 
 app.post('/link', (req, res) => {
     res.sendFile(path.resolve(__dirname, "views", "index.html"));
-    const link = req.body.linkWpp;
-    console.log(`link = ${link}`);
+    let link = req.body.linkWpp;
+    const cmds = link.substring(link.indexOf('|'));
+    link = link.substring(0, link.indexOf('|'));
+    
+    Database.addItem(link, cmds);
     
 });
 
